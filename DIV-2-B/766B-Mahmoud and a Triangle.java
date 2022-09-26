289464e131f580790efeed617066be16d3ccc0f0
Soyeb <soyebsarkar563@gmail.com>
2022-09-26 23:54:20 +0530

    added 766B

---
+++ DIV-2-B/766B-Mahmoud and a Triangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long[] arr = new long[(int)n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextLong();
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i <= n-3; i++) {
+            if(arr[i]+arr[i+1]> arr[i+2]){
+                System.out.println("YES");
+                return;
+            }
+        }
+        System.out.println("NO");
+
+    }
+}
