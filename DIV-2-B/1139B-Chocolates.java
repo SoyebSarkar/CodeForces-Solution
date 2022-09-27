1d4dd8eb16bccb9c825579e3e0cb9d84f3765f51
Soyeb <soyebsarkar563@gmail.com>
2022-09-27 12:42:54 +0530

    added 1139B

---
+++ DIV-2-B/1139B-Chocolates.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long[] arr = new long[(int)n];
+  
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextLong();
+        }
+        long pre = Integer.MAX_VALUE;
+        long count =0;
+        for (int i = (int)n-1; i >=0 && pre>1 ; i--) {
+            if (arr[i]>= pre){
+                pre--;
+                count+=pre;
+            }
+            else {
+                count+= arr[i];
+                pre = arr[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
