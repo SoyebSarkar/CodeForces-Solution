b981a6d02995d42aea276bfffab552e14216a494
Soyeb <soyebsarkar563@gmail.com>
2022-09-27 14:11:15 +0530

    added 991

---
+++ DIV-2-B/991B-Getting an A.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long[] arr = new long[(int)n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextLong();
+            sum+=arr[i];
+        }
+
+        if((1.0)*sum/n>=4.5){
+            System.out.println(0);
+            return;
+        }
+        long req = Math.round((4.5*n)-sum);
+        Arrays.sort(arr);
+        long count=0;
+        for (int i = 0; i < n && req>0; i++) {
+            long workLeft = 5-arr[i];
+            if (workLeft==0)continue;
+            if(workLeft>=req){
+                req-=req;
+
+            }
+            else {
+                req-=workLeft;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
