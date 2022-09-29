2235c6323136ebcd4c1735e6383a45366c58a9fa
Soyeb <soyebsarkar563@gmail.com>
2022-09-29 12:01:59 +0530

    added 841-B

---
+++ DIV-2-B/841B-Godsend.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int odd=0;
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            if(x%2==1){
+                odd++;
+        }
+        }
+        if(odd>0){
+            System.out.println("First");
+        }
+        else{
+            System.out.println("Second");
+        }
+    }
+}
