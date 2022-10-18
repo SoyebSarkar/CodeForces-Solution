457f3f9b66f4aab3edaad73aaa3dce22a62bb8b6
Soyeb <soyebsarkar563@gmail.com>
2022-10-18 23:17:08 +0530

    added A - Everyone Loves to Sleep

---
+++ DIV-2-A/A - Everyone Loves to Sleep.java
@@ -0,0 +1,158 @@
+
+import java.io.InputStreamReader;
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader(new BufferedInputStream(System.in));
+        /* For Multiple Test Cases  */
+        int times = sc.nextInt();
+        //        For single Test Case
+//        int times = 1;
+        while (times-- > 0){
+            solve(sc);
+
+        }
+
+
+
+    }
+    public static void solve(FastReader sc) {
+        int q = sc.nextInt();
+        int bedH = sc.nextInt();
+        int bedM = sc.nextInt();
+        long minTime = Integer.MAX_VALUE;
+        for (int i = 0; i < q; i++) {
+            int tempH = bedH;
+            long min = 0;
+
+            int qHour = sc.nextInt();
+            int qMin = sc.nextInt();
+
+            if (qMin >= bedM){
+                min+=qMin-bedM;
+            }
+            else {
+                min+= (60-bedM);
+                min+=qMin;
+                tempH = (tempH+1==24)?0:tempH+1;
+            }
+            if (qHour>= tempH){
+                min+= (qHour-tempH)* 60L;
+            }
+            else {
+                min+= (24-tempH)* 60L;
+                min+=qHour* 60L;
+            }
+            minTime = min(min,minTime);
+
+        }
+        out.println((minTime/60)+" "+(minTime%60));
+
+
+
+    }
+
+/*---------------------------------------------------------------------------------------
+        This part was copied from MagentaCobra 's  Template
+                    CodeForces ID - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+    static long maxSubArraySum(Deque<Long> a)
+    {
+        long size = a.size();
+        long tempMax = Integer.MIN_VALUE, maxEnd
+                = 0;
+
+        for (int i = 0; i < size; i++) {
+            maxEnd = maxEnd + a.pollFirst();
+            if (tempMax < maxEnd)
+                tempMax = maxEnd;
+            if (maxEnd < 0)
+                maxEnd = 0;
+        }
+        return tempMax;
+    }
+    /*-----------------------------------------------------------------------------------------------
+                This part was found randomly on Codeforces forgot to note author's Name  :(
+                                          Class For Fast I/O
+    ------------------------------------------------------------------------------------------------        */
+    private static class FastReader {
+        public BufferedReader br;
+        public StringTokenizer st;
+
+        public FastReader(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+            st = null;
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+
+
+
+}
