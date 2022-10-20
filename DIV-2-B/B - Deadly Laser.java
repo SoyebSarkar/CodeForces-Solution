b68742a1aa52b5151fe5f21f72929ee496d77d54
Soyeb <soyebsarkar563@gmail.com>
2022-10-20 22:44:11 +0530

    added B - Deadly Laser

---
+++ DIV-2-B/B - Deadly Laser.java
@@ -0,0 +1,142 @@
+
+
+import java.io.InputStreamReader;
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader(new BufferedInputStream(System.in));
+        /* For Multiple Test Cases  */
+        int times = sc.nextInt();
+        //        For single Test Case
+//        int times = 1;
+        while (times-- > 0){
+            solve(sc);
+
+        }
+
+
+
+    }
+    public static void solve(FastReader sc) {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int rowI = sc.nextInt();
+        int colI = sc.nextInt();
+        int d = sc.nextInt();;
+
+        int left = abs(1-colI);
+        int right = m -colI;
+        int up = abs(1-rowI);
+        int down = n-rowI;
+        if ((up>d && right>d) || (left>d && down>d)){
+            out.println((n-1)+(m-1));
+        }
+        else {
+            out.println(-1);
+        }
+    }
+
+/*---------------------------------------------------------------------------------------
+        This part was copied from MagentaCobra 's  Template
+                    CodeForces ID - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(long[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Long> ls = new ArrayList<Long>();
+        for(long x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+    static long maxSubArraySum(Deque<Long> a)
+    {
+        long size = a.size();
+        long tempMax = Integer.MIN_VALUE, maxEnd
+                = 0;
+
+        for (int i = 0; i < size; i++) {
+            maxEnd = maxEnd + a.pollFirst();
+            if (tempMax < maxEnd)
+                tempMax = maxEnd;
+            if (maxEnd < 0)
+                maxEnd = 0;
+        }
+        return tempMax;
+    }
+    /*-----------------------------------------------------------------------------------------------
+                This part was found randomly on Codeforces forgot to note author's Name  :(
+                                          Class For Fast I/O
+    ------------------------------------------------------------------------------------------------        */
+    private static class FastReader {
+        public BufferedReader br;
+        public StringTokenizer st;
+
+        public FastReader(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+            st = null;
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+
+
+
+}
