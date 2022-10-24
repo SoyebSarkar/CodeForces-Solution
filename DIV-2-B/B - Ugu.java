0047f0b100c1d642eb7229def4c543c9df3736b2
Soyeb <soyebsarkar563@gmail.com>
2022-10-24 20:15:15 +0530

    Added B - Ugu

---
+++ DIV-2-B/B - Ugu.java
@@ -0,0 +1,149 @@
+
+
+import java.io.InputStreamReader;
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader(new BufferedInputStream(System.in));
+        /* For Multiple Test Cases  */
+        int times = sc.nextInt();
+        //        For single Test Case
+//        int times = 1;
+        while (times-- > 0){
+            solve(sc);
+
+        }
+
+
+
+    }
+    public static void solve(FastReader sc) {
+        int n = sc.nextInt();
+        char[] arr = sc.next().toCharArray();
+        int op = 0;
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i]=='1'  && arr[i+1]=='0'){
+                op++;
+                i = i+1;
+                while (i<n-1){
+                    if (arr[i]!=arr[i+1])op++;
+                    i++;
+                }
+            }
+        }
+        out.println(op);
+    }
+
+
+
+
+/*---------------------------------------------------------------------------------------
+                            Template Credit - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+    static int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+
+        else
+            return gcd(b, a % b);
+    }
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+    static long maxSubArraySum(Deque<Long> a)
+    {
+        long size = a.size();
+        long tempMax = Integer.MIN_VALUE, maxEnd
+                = 0;
+
+        for (int i = 0; i < size; i++) {
+            maxEnd = maxEnd + a.pollFirst();
+            if (tempMax < maxEnd)
+                tempMax = maxEnd;
+            if (maxEnd < 0)
+                maxEnd = 0;
+        }
+        return tempMax;
+    }
+    /*-----------------------------------------------------------------------------------------------
+                                          Class For Fast I/O
+    ------------------------------------------------------------------------------------------------        */
+    private static class FastReader {
+        public BufferedReader br;
+        public StringTokenizer st;
+
+        public FastReader(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+            st = null;
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+
+
+
+}
