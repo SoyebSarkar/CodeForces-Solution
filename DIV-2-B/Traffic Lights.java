d902403f48f1631b0296a18682fd23883cf6cc25
Soyeb <soyebsarkar563@gmail.com>
2022-10-30 23:30:10 +0530

    Added b

---
+++ DIV-2-B/Traffic Lights.java
@@ -0,0 +1,154 @@
+
+
+import java.io.InputStreamReader;
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader(new BufferedInputStream(System.in));
+        /* For Multiple Test Cases  */
+        int times = sc.nextInt();
+        //        For single Test Case
+//        int times = 1;
+        while (times-- > 0){
+            solve(sc);
+
+        }
+
+
+
+    }
+    public static void solve(FastReader sc) {
+        int n = sc.nextInt();
+        char ch = sc.next().charAt(0);
+        StringBuilder str = new StringBuilder(sc.next());
+        if (ch=='g'){
+            out.println(0);
+            return;
+        }
+        int i = 0;
+        int j = 1;
+        int max = 0;
+        while (i<n){
+            while (i<n && str.charAt(i)!=ch)i++;
+            j = (i+1)%n;
+
+            while (j<n && str.charAt(j)!='g') j= (j+1)%n;
+            if (i<n && j<n){
+                if (i<j){
+                    max = max(max,(j-i));
+                }
+                else {
+                    max = max(max,(n-i)+j);
+                }
+            }
+            if (j<i)break;
+            i = j+1;
+        }
+        out.println(max);
+
+    }
+
+/*---------------------------------------------------------------------------------------
+        This part was copied from MagentaCobra 's  Template
+                    CodeForces ID - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+    static long maxSubArraySum(Deque<Long> a)
+    {
+        long size = a.size();
+        long tempMax = Integer.MIN_VALUE, maxEnd
+                = 0;
+
+        for (int i = 0; i < size; i++) {
+            maxEnd = maxEnd + a.pollFirst();
+            if (tempMax < maxEnd)
+                tempMax = maxEnd;
+            if (maxEnd < 0)
+                maxEnd = 0;
+        }
+        return tempMax;
+    }
+    /*-----------------------------------------------------------------------------------------------
+                This part was found randomly on Codeforces forgot to note author's Name  :(
+                                          Class For Fast I/O
+    ------------------------------------------------------------------------------------------------        */
+    private static class FastReader {
+        public BufferedReader br;
+        public StringTokenizer st;
+
+        public FastReader(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+            st = null;
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+
+
+
+}
