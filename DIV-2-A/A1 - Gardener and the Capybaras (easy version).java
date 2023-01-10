9722d716390a9bc5a0d56e5f81d2c9314430ba21
Soyeb <soyebsarkar563@gmail.com>
2023-01-10 23:14:38 +0530

    Added A1 Gardener and the Capybaras (easy version)

---
+++ DIV-2-A/A1 - Gardener and the Capybaras (easy version).java
@@ -0,0 +1,178 @@
+
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import static java.lang.System.out;
+
+public class Main {
+
+    static long mod = (long)1e9+7;
+
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+
+            int testcases = in.nextInt();
+//             int testcases = 1;
+
+            while ( testcases -- > 0 ) {
+//---------------------------------------------------------
+              String str = in.next();
+              boolean br = false;
+                for (int i = 0; i < str.length(); i++) {
+                    for (int j = str.length()-1; j >i+1 ; j--) {
+                        String first = str.substring(0,i+1);
+
+                        String second = str.substring(i+1,(j-i));
+                        String third = str.substring(j,(str.length()));
+                        if((second.compareTo(first)<=0 && second.compareTo(third)<=0) || (second.compareTo(first)>=0 && second.compareTo(third)>=0)){
+                            out.println(first+" "+second+" "+third);
+                            br = true;
+                            break;
+                        }
+                    }
+                    if (br)break;
+                }
+
+
+
+// ---------------------------------------------------
+            }
+
+            out.close();
+        } catch (Exception e) {
+            out.println(e);
+            return;
+        }
+    }
+
+    private static void reverse(int[] arr) {
+        int i =0;
+        int j = arr.length-1;
+        while (i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] =temp;
+            i++;
+            j--;
+        }
+    }
+
+/*---------------------------------------------------------------------------------------
+                            Template Credit - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void sort(long[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Long> ls = new ArrayList<Long>();
+        for(long x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+    static long gcd(long a, long b)
+    {
+        if (b == 0)
+            return a;
+
+        else
+            return gcd(b, a % b);
+    }
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+
+
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+}
+class Pair {
+    long f;
+    long s;
+    Pair(long h, long s){
+        this.f = h;
+        this.s = s;
+    }
+}
