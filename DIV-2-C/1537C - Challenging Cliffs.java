3d35bf088d24824b6e2774d21a25c9557c5911c4
Soyeb <soyebsarkar563@gmail.com>
2023-01-30 20:46:33 +0530

    Added 1537C - Challenging Cliffs

---
+++ DIV-2-C/1537C - Challenging Cliffs.java
@@ -0,0 +1,236 @@
+
+
+import static java.lang.Math.*;
+
+import java.util.*;
+import java.io.*;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    static ArrayList<Integer> prime = new ArrayList<>();
+    static long mod = 1000000007;
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+
+            int testcases = in.nextInt();
+//             int testcases = 1;
+
+            while (testcases-- > 0) {
+//---------------------------------------------------------
+               int n = in.nextInt();
+               long[] ar = new long[n];
+               HashSet<Long> hashSet = new HashSet<>();
+               long dup = -1;
+                for (int i = 0; i < n; i++) {
+                    ar[i] = in.nextLong();
+
+                }
+                sort(ar);
+                long min1 = 0;
+                long min2 = 0;
+                long diff = Long.MAX_VALUE;
+                for (int i = 0; i < n; i++) {
+                    if (hashSet.contains(ar[i])){
+                        dup=ar[i];
+                        break;
+                    }
+                    if (i>0){
+                        if (ar[i]-ar[i-1]<diff){
+                            diff = ar[i]-ar[i-1];
+                            min1 = ar[i];
+                            min2 = ar[i-1];
+                        }
+                    }
+                    hashSet.add(ar[i]);
+                }
+                if (dup !=-1){
+                    out.print(dup+" ");
+                    int cnt = 2;
+                    PriorityQueue<Long> pq = new PriorityQueue<>();
+                    for (int i = 0; i < n; i++) {
+                        if(dup==ar[i] && cnt>0){
+                            cnt--;
+                            continue;
+                        }
+                        if (ar[i]<=dup){
+                            pq.add(ar[i]);
+                            continue;
+                        }
+                        out.print(ar[i]+" ");
+                    }
+                    while (!pq.isEmpty())out.print(pq.remove()+" ");
+                    out.println(dup);
+                    continue;
+                }
+
+                    out.print(min2+" ");
+                PriorityQueue<Long> pq = new PriorityQueue<>();
+                    for (int i = 0; i < n; i++) {
+                        if (ar[i]==min1 || ar[i]==min2)continue;
+                        if (ar[i]<=min2){
+                            pq.add(ar[i]);
+                            continue;
+                        }
+                        out.print(ar[i]+" ");
+                    }
+                while (!pq.isEmpty())out.print(pq.remove()+" ");
+                    out.println(min1);
+
+
+
+
+// ---------------------------------------------------
+        }
+            out.close();
+        } catch (Exception e) {
+            out.println(e);
+            return;
+       }
+    }
+
+
+
+/*---------------------------------------------------------------------------------------
+                            Template Credit - MagentaCobra
+--------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void sort(long[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Long> ls = new ArrayList<Long>();
+        for(long x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static void print(int[] arr)
+    {
+        //for debugging only
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+    static long gcd(long a, long b)
+    {
+        if (b == 0)
+            return a;
+
+        else
+            return gcd(b, a % b);
+    }
+    static int fact(int n)
+    {
+        if(n==0)
+            return 1;
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+            res = res * i;
+        return res;
+    }
+    static int nCr(int n, int r)
+    {
+        return fact(n) / (fact(r) *
+                fact(n - r));
+    }
+    static long power(long a, long n){
+        long res = 1;
+        while (n>0){
+            if (n%2==1){
+                res = (res*a)%mod;
+                n--;
+            }
+            else {
+                a = (a * a) % mod;
+                n/=2;
+            }
+        }
+        return res;
+    }
+
+
+    /*-----------------------------------------------------------------------------------------------
+            End for MagentaCobra's Template
+    ------------------------------------------------------------------------------------------------        */
+
+
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+}
+class Pair {
+    long f;
+    long s;
+    Pair(long h, long s){
+        this.f = h;
+        this.s = s;
+    }
+}
