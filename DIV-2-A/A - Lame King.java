1b2307ef4ec8a430ec734ddcc1a4279e4e1c965e
Soyeb <soyebsarkar563@gmail.com>
2023-03-12 23:12:04 +0530

    Added new problem of  (Div. 1 + Div. 2)

---
+++ DIV-2-A/A - Lame King.java
@@ -0,0 +1,267 @@
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+        static long mod = 1000000007;
+
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+//          Author: Soyeb Sarkar
+
+
+            int testcases = in.nextInt();
+//            int testcases = 1;
+            while (testcases-- > 0) {
+                //---------------------------------------------------------
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int tx = 0;
+                int ty = 0;
+                int tot = 0;
+                boolean turn = true;
+                if (abs(y)>abs(x)){
+                    int t = y;
+                    y = x;
+                    x = t;
+                }
+                int time = 0;
+                while (tx!=x || ty!=y){
+                    if (turn){
+                        if (x>tx)tx++;
+                        else if (x<tx)tx--;
+                    }
+                    else {
+                        if (y>ty)ty++;
+                        else if (y<ty)ty--;
+                    }
+                    turn = !turn;
+                    time++;
+                }
+                out.println(time);
+
+
+
+                // ---------------------------------------------------
+            }
+            out.close();
+        } catch (Exception e) {
+            out.println(e);
+            return;
+        }
+    }
+
+
+    /*---------------------------------------------------------------------------------------
+                                Extra Methods
+    --------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr) {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for (int x : arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    private static int upper_bound(long[] arr,int low, int high, long target){
+        int n = arr.length;
+        int mid = 0;
+//        int low = 0;
+//        int high = n;
+        while (low < high && low != n) {
+            mid = low + (high - low) / 2;
+
+            if (target >= arr[mid]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+
+    }
+    static int lower_bound(long arr[],int low, int high, long key)
+    {
+
+        int mid;
+
+        while (low < high) {
+
+            mid = low + (high - low) / 2;
+            if (key <= arr[mid]) {
+                high = mid;
+            }
+
+            else {
+
+                low = mid + 1;
+            }
+        }
+
+        if (low < arr.length && arr[low] < key) {
+            low++;
+        }
+
+        return low;
+    }
+//    Only Use this for floor value
+//    private static long sqrt(long num) {
+//        long l = 0;
+//        long h = Integer.MAX_VALUE;
+//        long mid;
+//        while (h - l > 1) {
+//            mid = (h + l)/2;
+//
+//            if (mid*mid <= num) {
+//                l = mid;
+//            }else {
+//                h = mid;
+//            }
+//        }
+//        return l;
+//    }
+
+    public static void sort(long[] arr) {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Long> ls = new ArrayList<Long>();
+        for (long x : arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+
+    public static void print(int[] arr) {
+        //for debugging only
+        for (int x : arr)
+            out.print(x + " ");
+        out.println();
+    }
+
+    static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+
+        else
+            return gcd(b, a % b);
+    }
+
+    static long fact(long n) {
+        if (n == 0)
+            return 1;
+        long res = 1;
+        for (int i = 2; i <= n; i++)
+            res = (res * i)%mod;
+        return res;
+    }
+
+    static long nCr(long n, long r) {
+        return fact(n) / (fact(r) *
+                fact(n - r));
+    }
+
+    static long power(long a, long n) {
+        long res = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res = (res * a) % mod;
+                n--;
+            } else {
+                a = (a * a) % mod;
+                n /= 2;
+            }
+        }
+        return res;
+    }
+
+
+        /*-----------------------------------------------------------------------------------------------
+                                        - End -
+        ------------------------------------------------------------------------------------------------        */
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+}
+
+class Pair{
+    int fe;
+    int se;
+    Pair(int f, int s){
+        this.fe = f;
+        this.se = s;
+    }
+}
---
+++ DIV-2-B/B - Vaccination.java
@@ -0,0 +1,265 @@
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+        static long mod = 1000000007;
+
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+//          Author: Soyeb Sarkar
+
+
+            int testcases = in.nextInt();
+//            int testcases = 1;
+            while (testcases-- > 0) {
+                //---------------------------------------------------------
+                int n = in.nextInt();
+                int pack = in.nextInt();
+                int expMoment = in.nextInt();
+                int wait = in.nextInt();
+                int[] ar = new int[n];
+                for (int i = 0; i < n; i++) {
+                    ar[i] = in.nextInt();
+                }
+                int temp = 0;
+                int tot = 0;
+                int low = 0;
+                int high = 0;
+                for (int i = 0; i < n; i++) {
+                    if (temp==0 || ar[i]>high){
+                        tot++;
+                        temp = pack;
+                        low = ar[i];
+                        high = ar[i]+wait+expMoment;
+                    }
+                    if (ar[i]>=low && ar[i]<=high){
+                        temp--;
+                    }
+                }
+                out.println(tot);
+
+
+                // ---------------------------------------------------
+            }
+            out.close();
+        } catch (Exception e) {
+            out.println(e);
+            return;
+        }
+    }
+
+
+    /*---------------------------------------------------------------------------------------
+                                Extra Methods
+    --------------------------------------------------------------------------------------- */
+
+    public static void sort(int[] arr) {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for (int x : arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    private static int upper_bound(long[] arr,int low, int high, long target){
+        int n = arr.length;
+        int mid = 0;
+//        int low = 0;
+//        int high = n;
+        while (low < high && low != n) {
+            mid = low + (high - low) / 2;
+
+            if (target >= arr[mid]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+
+    }
+    static int lower_bound(long arr[],int low, int high, long key)
+    {
+
+        int mid;
+
+        while (low < high) {
+
+            mid = low + (high - low) / 2;
+            if (key <= arr[mid]) {
+                high = mid;
+            }
+
+            else {
+
+                low = mid + 1;
+            }
+        }
+
+        if (low < arr.length && arr[low] < key) {
+            low++;
+        }
+
+        return low;
+    }
+//    Only Use this for floor value
+//    private static long sqrt(long num) {
+//        long l = 0;
+//        long h = Integer.MAX_VALUE;
+//        long mid;
+//        while (h - l > 1) {
+//            mid = (h + l)/2;
+//
+//            if (mid*mid <= num) {
+//                l = mid;
+//            }else {
+//                h = mid;
+//            }
+//        }
+//        return l;
+//    }
+
+    public static void sort(long[] arr) {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Long> ls = new ArrayList<Long>();
+        for (long x : arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+
+    public static void print(int[] arr) {
+        //for debugging only
+        for (int x : arr)
+            out.print(x + " ");
+        out.println();
+    }
+
+    static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+
+        else
+            return gcd(b, a % b);
+    }
+
+    static long fact(long n) {
+        if (n == 0)
+            return 1;
+        long res = 1;
+        for (int i = 2; i <= n; i++)
+            res = (res * i)%mod;
+        return res;
+    }
+
+    static long nCr(long n, long r) {
+        return fact(n) / (fact(r) *
+                fact(n - r));
+    }
+
+    static long power(long a, long n) {
+        long res = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res = (res * a) % mod;
+                n--;
+            } else {
+                a = (a * a) % mod;
+                n /= 2;
+            }
+        }
+        return res;
+    }
+
+
+        /*-----------------------------------------------------------------------------------------------
+                                        - End -
+        ------------------------------------------------------------------------------------------------        */
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+}
+
+class Pair{
+    int fe;
+    int se;
+    Pair(int f, int s){
+        this.fe = f;
+        this.se = s;
+    }
+}
